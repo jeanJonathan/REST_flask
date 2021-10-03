3bcaf14f3496061906a940ff3def9add5ffb4e9c
stephane isnard <stephane.isnard@univ-tlse2.fr>
2021-10-03 14:53:43 +0200

    add bd requests to get regions and departements lists

--- api/app.py
+++ api/app.py
@@ -1,5 +1,33 @@
 from flask import Flask, jsonify
+import mariadb
 app = Flask(__name__)
+
+app.config['JSON_AS_ASCII'] = False  # pour utiliser l'UTF-8 plutot que l'unicode
+
+config = {
+    'host': 'mariadb',
+    'port': 3306,
+    'user': 'root',
+    'password': 'root',
+    'database': 'mydatabase'
+}
+
+
+def execute_query(query):
+    # connection for MariaDB
+    conn = mariadb.connect(**config)
+    # create a connection cursor
+    cur = conn.cursor()
+    # execute a SQL statement
+    cur.execute(query)
+
+    # serialize results into JSON
+    row_headers = [x[0] for x in cur.description]
+    rv = cur.fetchall()
+    json_data = []
+    for result in rv:
+        json_data.append(dict(zip(row_headers, result)))
+    return json_data
 
 
 # we define the route /
@@ -11,14 +39,15 @@ def welcome():
 
 @app.route('/regions')
 def get_regions():
-    # return a json
-    return jsonify({'status': 'api working regions'})
+    json_data = execute_query("select * from regions")
+    return jsonify(json_data)
 
 
 @app.route('/departements')
 def get_departements():
+    json_data = execute_query("select * from departements")
     # return a json
-    return jsonify({'status': 'api working departements'})
+    return jsonify(json_data)
 
 
 @app.route('/villes')
